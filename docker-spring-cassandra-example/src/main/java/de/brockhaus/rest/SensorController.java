449df35e1a267e7031ebf1bbc6191ca3bad08d23
OUFTOU <OUFTOU@OUFTOU-PC>
2016-01-22 18:26:19 +0000

    initial

--- docker-spring-cassandra-example/src/main/java/de/brockhaus/rest/SensorController.java
+++ docker-spring-cassandra-example/src/main/java/de/brockhaus/rest/SensorController.java
@@ -13,33 +13,33 @@ import de.brockhaus.domain.Sensor;
 import de.brockhaus.domain.SensorRepository;
 
 /**
- * @author Z.OUFTOU
+ * @author zouftou
  */
 @RestController
 public class SensorController {
-	
+
 	@Autowired
 	private SensorRepository sensorRepo;
-	
+
 	@PostConstruct
-	public void initData(){
+	public void initData() {
 		sensorRepo.deleteAll();
-		sensorRepo.save(new Sensor("0001","oilpression"));
-		sensorRepo.save(new Sensor("0002","temperature"));
+		sensorRepo.save(new Sensor("0001", "oilpression"));
+		sensorRepo.save(new Sensor("0002", "temperature"));
 	}
-	
+
 	@RequestMapping("/")
-	public String index(){
+	public String index() {
 		return "<center></br><h1>*** Brockhaus Gruppe ***</h1></br><h2> Docker Spring Cassandra Example</h2></center>";
 	}
-	
-    @RequestMapping(value = "/sensor/{name}")
-    public Sensor getSensor(@PathVariable("name") String pName) {
-    	return (Sensor) sensorRepo.findByName(pName,1);
-    }
-    
-    @RequestMapping("/sensors")
-	public List<Sensor> getAll(){
+
+	@RequestMapping(value = "/sensor/{name}")
+	public Sensor getSensor(@PathVariable("name") String pName) {
+		return (Sensor) sensorRepo.findByName(pName, 1);
+	}
+
+	@RequestMapping("/sensors")
+	public List<Sensor> getAllSensors() {
 		return (List<Sensor>) sensorRepo.findAll();
 	}
 }
