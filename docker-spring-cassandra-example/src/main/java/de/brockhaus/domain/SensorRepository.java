21c3c06ade41cc5e5c89d2b358814401e331334c
Zouhir OUFTOU <zouhir.ouftou@gmail.com>
2016-02-01 20:19:30 +0000

    initial

--- docker-spring-cassandra-example/src/main/java/de/brockhaus/domain/SensorRepository.java
+++ docker-spring-cassandra-example/src/main/java/de/brockhaus/domain/SensorRepository.java
@@ -3,6 +3,9 @@ package de.brockhaus.domain;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.data.cassandra.repository.Query;
 
+/**
+ * @author z.ouftou
+ */
 public interface SensorRepository extends CassandraRepository<Sensor> {
 	
     @Query("SELECT * FROM sensors WHERE name=?0")
